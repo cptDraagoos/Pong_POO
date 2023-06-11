f5a75d81fda83e95e5987c205951184f1225eef4
cptDraagoos <119045959+cptDraagoos@users.noreply.github.com>
2023-06-11 13:27:11 +0300

    Add files via upload
    add star menu

--- main.cpp
+++ main.cpp
@@ -8,7 +8,18 @@ using namespace std;
 
 int main()
 {
+    int ch;
     GameMgm c(40,20);
-    c.Run();
+    cout<<"While playing the game you can press 'q' to close the game";
+    cout<<"\nPress 1 to start playing PONG or press 2 to close the program:";
+    cin>>ch;
+    switch (ch)
+    {
+    case 1:
+        c.Run();
+        break;
+    case 2:
+        break;
+    }
     return 0;
 }
